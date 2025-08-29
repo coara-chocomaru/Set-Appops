4dffa97d2bb18c4556bb8854614714347199755d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-29 18:32:20 +0900

    Update appops.sh

--- app/src/main/assets/appops.sh
+++ app/src/main/assets/appops.sh
@@ -3,8 +3,8 @@
 PACKAGES=$(pm list packages | cut -d':' -f2)
 
 for pkg in $PACKAGES; do
-    sh -c "appops set \"$pkg\" REQUEST_INSTALL_PACKAGES allow 2>/dev/null"
-    sleep 1 
-    sh -c "appops get \"$pkg\" REQUEST_INSTALL_PACKAGES 2>/dev/null"
-    sleep 1 
+    appops set "$pkg" REQUEST_INSTALL_PACKAGES allow 2>/dev/null
+    appops get "$pkg" REQUEST_INSTALL_PACKAGES 2>/dev/null
 done
+
+echo "処理が完了しました！ アプリを終了してください"
